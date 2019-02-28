f9b5d5781003675b4b22bd58336c293d238b8c90
Erivks <mordecai.es27@gmail.com>
2019-02-27 23:40:32 -0300

    Exercicio de POO de conta bancária

---
+++ POO/POOV/Exercises/bank.py
@@ -0,0 +1,34 @@
+'''SUPERCLASS'''
+class Bank(object):
+    __total_amount = 1000000
+    booking_fee = .1
+    __required_reserve = 10000
+    def __calculateReserve(self):
+        return Bank.__total_amount * Bank.booking_fee
+    def canLoan(self, value):
+        reserve = self.__calculateReserve()
+        if reserve - value > Bank.__required_reserve:
+            return True
+        else:
+            return False
+    
+'''SUBCLASS'''
+class Account(Bank):
+    def __init__(self, id, password):
+        self.__balance = 50
+        self.__id = id
+        self.__password = password
+    def correctPassword(self, password):
+        if password == self.__password:
+            return True
+        else:
+            return False
+    def deposit(self, password, value):
+        if password == self.__password:
+            print('The password is correct.')
+            self.__balance += value
+            Bank.__total_amount += value
+            print('Deposit made succefull')
+        else:
+            print('Password is incorrect.')
+    
