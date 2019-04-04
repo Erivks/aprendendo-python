8c4bb038215d6cbcdd6abd896adb681a246cb958
Erivks <mordecai.es27@gmail.com>
2019-04-04 11:12:12 -0300

    Peso ideal para homens e mulheres

---
+++ Exercicios/EstruturaSequencial/WeightMF.py
@@ -0,0 +1,4 @@
+height = float(input("Type your height: "))
+mWeight = (72.7 * height) - 58
+fWeight = (62.1 * height) - 44.7
+print("Men's ideal weight: {}\nWoman's ideal weight: {}".format(mWeight, fWeight))
